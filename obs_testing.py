6f275d56479ce9924608a07760e5919c1c659bd6
jtwwang <justin.w.wang.0@gmail.com>
2019-02-26 16:40:48 -0800

    Finished parse_observations for one obs

--- obs_testing.py
+++ obs_testing.py
@@ -51,20 +51,21 @@ class Runner(object):
 		# Current player
 		print('------------ {} -------------'.format(keys[1]))
 		val = obs[keys[1]]
-		print('{}: {}'.format(type(val),val))
+		print('{}: {}\n\n'.format(type(val),val))
 
 	def parse_observations(observations, num_actions, obs_stacker):
-		"""Deconstructs the rich observation data into relevant components.
+		""" PULLED FROM rainbow/run_experiment.py, FOR REFERENCE ONLY
+		Deconstructs the rich observation data into relevant components.
 		Args:
-		observations: dict, containing full observations.
-		num_actions: int, The number of available actions.
-		obs_stacker: Observation stacker object.
+			observations: dict, containing full observations.
+			num_actions: int, The number of available actions.
+			obs_stacker: Observation stacker object.
 		Returns:
-		current_player: int, Whose turn it is.
-		legal_moves: `np.array` of floats, of length num_actions, whose elements
-			are -inf for indices corresponding to illegal moves and 0, for those
-			corresponding to legal moves.
-		observation_vector: Vectorized observation for the current player.
+			current_player: int, Whose turn it is.
+			legal_moves: `np.array` of floats, of length num_actions, whose elements
+				are -inf for indices corresponding to illegal moves and 0, for those
+				corresponding to legal moves.
+			observation_vector: Vectorized observation for the current player.
 		"""
 		current_player = observations['current_player']
 		current_player_observation = (
@@ -76,7 +77,24 @@ class Runner(object):
 		observation_vector = obs_stacker.get_observation_stack(current_player)
 		return current_player, legal_moves, observation_vector
 
+	def parse_observations(obs):
+		""" Parses current observation
+		Args:
+			obs (dict): Full observations
+		Returns:
+			curr_player (int): Whose turn it is.
+			legal_moves (np.array of floats): Illegal moves are -inf, and legal moves are 0.
+			obs_vector (list): Vectorized observation for current player.
+		"""
+		curr_player = obs['current_player']
+		curr_player_obs = obs['player_observations'][current_player]
+		legal_moves = curr_player_obs['legal_moves_as_int']
+		obs_vector = curr_player_obs['vectorized']
+
+		return curr_player, legal_moves, obs_vector
+
+
 if __name__ == "__main__":
-	runner = Runner(2,1)
+	runner = Runner(3,1)
 	runner.run()
 	#runner.run()
