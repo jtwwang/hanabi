4d2202d23566076e1598734116fc286d7e2f1806
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-03-24 15:38:34 -0700

    corrected baysian probability, compatible with vectorized

--- baysian_belief.py
+++ baysian_belief.py
@@ -4,17 +4,25 @@ import pyhanabi
 
 class Belief():
 
-    def __init__(self, n_players):
+    color_map = {
+        'Y': 0,
+        'B': 1,
+        'R': 2,
+        'W': 3,
+        'G': 4
+    }
+
+    def __init__(self, players):
         """
-        n_players: (int) the number of players in the game
+        players: (int) the number of players in the game
         """
-        if n_players <= 3:
+        if players <= 3:
             self.n_cards = 5
         else:
             self.n_cards = 4
-        self.n_players = n_players
+        self.players = players
 
-        self.belief = np.zeros((n_players * self.n_cards + 1, 5, 5))
+        self.belief = np.zeros((players * self.n_cards + 1, 5, 5))
         self.discard_belief = np.zeros((5, 10))
 
     def reset(self):
@@ -29,8 +37,8 @@ class Belief():
             index (int): which we are inserting
             card (object): the characteristics of the card to insert
         """
-        color = card.color()
-        rank = card.rank()
+        color = self.color_map[card['color']]
+        rank = card['rank']
 
         # set the probability for that specific number to 1
         # because we know it exists
@@ -43,8 +51,8 @@ class Belief():
             this matrix is the exact knowledge of the discard pile
         """
         # extract color and rank of the card
-        color = card.color()
-        rank = card.rank()
+        color = self.color_map[card['color']]
+        rank = card['rank']
 
         # place the card in the correct spot
         if rank == 0 and not np.any(self.belief[:, color, 0]):
@@ -59,9 +67,10 @@ class Belief():
 
     def insert_many(self, index, color, rank):
         # all cards until the rank become 1
+        color = self.color_map[color]
         self.belief[index, color, :rank] = 1
 
-    def calculate_prob(self, player, card_knowledge):
+    def calculate_prob(self, player_id, obs):
         """
         this function calculates the probability distribution of each card
         over the possible colors and possible ranks
@@ -70,34 +79,33 @@ class Belief():
         card_knowledge (object)
 
         """
-        # calculate the probability for each card
-        rank_p = np.zeros(10)
-
         full_known = np.sum(self.belief, axis=0)
         availability = [3, 2, 2, 2, 1]
-        index = player * self.n_cards
+        index = player_id * self.n_cards
 
+        # count the full availability of cards
         for color in range(5):
             for rank in range(5):
-                self.belief[index:index + self.n_cards, color,
-                            rank] = availability[rank] - full_known[color, rank]
+                self.belief[:self.n_cards, color, rank] = availability[rank] - full_known[color, rank]
 
         # filter by hint
-        for cid in range(self.n_cards):
-            for color in range(5):
-                my_card = card_knowledge[player][cid]
+        space_knowledge = 35 * self.n_cards
+        ix = len(obs) - (space_knowledge * (self.players - player_id))
+        from_vec = obs[ix:ix + 35 * self.n_cards]
+        mask = np.empty((self.n_cards,5,5), dtype=np.uint8)
+        for i in range(self.n_cards):
+            mask[i] = np.reshape(from_vec[i*35: i*35 + 25], (5,5))
 
-                # FIXME: the following line does not work
-                if not my_card.color_plausible(color):
-                    self.belief[cid + index, color, :] = 0
+        self.belief[index: index + self.n_cards] = np.multiply(self.belief[index:index + self.n_cards], mask)
 
         # divide by total
-        for card in self.belief[index:index + self.n_cards, :, :]:
-            card[:, :] /= card.sum()
+        for card in self.belief[index:index + self.n_cards]:
+            if card.sum() == 0:
+                print(card.sum())
+                exit()
+            card /= card.sum()
 
-        print(self.belief[index:index + self.n_cards])
-
-    def encode(self, observation, player):
+    def encode(self, obs, player_id):
         """
         Encodes the observations and the knowledge into the belief
             player (int) 
@@ -112,28 +120,25 @@ class Belief():
         self.reset()
 
         # update the firework knowledge
-        fireworks = observation.fireworks()
-        for color in range(len(fireworks)):
+        fireworks = obs['fireworks']
+        for color in fireworks:
             if fireworks[color] != 0:
                 # insert in the last spot
                 self.insert_many(self.n_cards *
-                                 self.n_players, color, fireworks[color])
+                                 self.players, color, fireworks[color])
 
         # update the discard knowledge
-        discard = observation.discard_pile()
+        discard = obs['discard_pile']
         for card in discard:
             self.insert_discard(card)
 
         # update the observed hand knowledge
-        observed = observation.observed_hands()
-        for p in range(len(observed)):
-            if p == player:
-                continue
-            else:
-                for c in range(len(observed[p])):
-                    card = observed[p][c]
-                    self.insert(p * self.n_cards + c, card)
+        ob = obs['vectorized']
+        from_vec = ob[:25*self.n_cards*(self.players - 1)]
+        mask = np.empty((self.n_cards,5,5), dtype=np.uint8)
+        for i in range(self.n_cards * (self.players - 1)):
+            self.belief[i + self.n_cards] = np.reshape(from_vec[i*25:(i+1)*25], (5,5))
 
-        self.calculate_prob(player, observation.card_knowledge())
+        self.calculate_prob(player_id, ob)
 
         return self.belief
