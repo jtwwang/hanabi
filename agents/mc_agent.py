e229d7d28ec99859bee96f1619275b6c50be289b
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-03-27 20:35:49 -0700

    work in progress in monte carlo agent

--- agents/mc_agent.py
+++ agents/mc_agent.py
@@ -11,7 +11,7 @@ from rl_env import Agent
 import numpy as np
 import baysian_belief
 import random
-import policy_prediction
+from policy_predictor import policy_net
 from datetime import datetime
 random.seed(datetime.now())
 
@@ -28,10 +28,13 @@ class MCAgent(Agent):
         self.belief = baysian_belief.Belief(config['players'])
 
         # load the predictor
-        pp = policy_predictor(config['observation_size'], config['num_moves'])
-        pp.load() #FIXME: LOAD A SPECIFIC MODEL, for the agent we are competing against
+        agent_class = 'SimpleAgent' #FIXME: temporary
+
+        pp = policy_net(config['observation_size'], config['num_moves'], agent_class)
+        pp.load()
 
     def sample(self,card):
+        """ sample a card from distribution"""
         rand = random.random()
         v = np.zeros(25)
         for r in range(card.shape[0]):
@@ -42,39 +45,56 @@ class MCAgent(Agent):
                 else:
                     rand -= card[r][c]
 
-    def next_state_from_move(self, move):
-        """returns the next state given a move"""
-
-    def encode(self, obs):
-        """returns an hashable state from the observation"""
-
-    def next_state_from_predictor(self, obs):
-        
-
-    def act(self, obs):
-        my_belief = self.belief.encode(obs, self.player_id)
+    def sampled_obs(self, my_belief, ob):
+        """ returns a sample of observations from belief and observations"""
         v_sample = np.zeros(0)
         for card in my_belief:
             v = self.sample(card)
             v_sample = np.append(v_sample,v)
 
+        raise NotImplementedError
+        return v_sample
+
+    def next_state_from_move(self, move, state):
+        """returns the next state given a move"""
+        raise NotImplementedError
+
+    def encode(self, ob):
+        """returns an hashable state from the observation"""
+        return str(ob)
+
+    def decode(self, state):
+        raise NotImplementedError
+
+    def next_state_from_predictor(self, state):
+        raise NotImplementedError
+
+    def act(self, obs):
+        my_belief = self.belief.encode(obs, self.player_id)
+        
         # create empty dictionary to save stats of all nodes
         stats = {}
 
         # set initial state, make it hashable
-        state = encode(obs)
+        self.root = self.encode(obs['vectorized'])
+        state = self.root
 
         # random rollouts
-        n_rollouts = 1000
-        for r in n_rollouts:
-            # my move
-            move = random.choice(obs['legal_moves'])
-            state = next_state_from_move()
+        rollouts = 1000
+        for r in range(rollouts):
+            depth = 0
+            while depth < self.max_depth:
+                # my move
+                move = random.choice(obs['legal_moves'])
+                state = self.next_state_from_move(move, state)
 
-            # other players moves
-            for p in self.config['players']:
-                # choose random sample
+                # other players moves
+                for p in self.config['players']:
+                    # choose random sample
+                    state = self.next_state_from_predictor(state)
 
+                # increase the depth by 1
+                depth += 1
         
         return random.choice(obs['legal_moves']) #FIXME: 
 
