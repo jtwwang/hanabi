2078d2e0541e2b3c7002c8c54420d97211dab419
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-04-03 14:16:13 -0700

    adapted encode function to work on vectorized observations

--- baysian_belief.py
+++ baysian_belief.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pyhanabi
-
+from state_translate import state_translator
 
 class Belief():
 
@@ -31,34 +31,8 @@ class Belief():
         """
         self.belief[:, :, :] = 0
         self.discard_belief[:, :] = 0
-
-    def insert_discard(self, card):
-        """
-            for the discard pile we don't need to know the specific order of the card
-            and / or if there are duplicates
-            this matrix is the exact knowledge of the discard pile
-        """
-        # extract color and rank of the card
-        color = self.color_map[card['color']]
-        rank = card['rank']
-
-        # place the card in the correct spot
-        if rank == 0 and not np.any(self.belief[:, color, 0]):
-            self.discard_belief[color, 0] = 1
-        else:
-            # place the card in the correct spot
-            # card 2 goes on 3 or 4, card 3 goes on 5 or 6, etc...
-            placement = 1 + (rank * 2)
-            if np.any(self.discard_belief[color, placement]):
-                placement += 1
-            self.discard_belief[color, placement] = 1
-
-    def insert_many(self, index, color, rank):
-        # all cards until the rank become 1
-        color = self.color_map[color]
-        self.belief[index, color, :rank] = 1
-
-    def calculate_prob(self, player_id, obs):
+    
+    def calculate_prob(self, player_id, vectorized):
         """
         this function calculates the probability distribution of each card
         over the possible colors and possible ranks
@@ -78,8 +52,8 @@ class Belief():
 
         # filter by hint
         space_knowledge = 35 * self.n_cards
-        ix = len(obs) - (space_knowledge * (self.players - player_id))
-        from_vec = obs[ix:ix + 35 * self.n_cards]
+        ix = len(vectorized) - (space_knowledge * (self.players - player_id))
+        from_vec = vectorized[ix:ix + 35 * self.n_cards]
         mask = np.empty((self.n_cards,5,5), dtype=np.uint8)
         for i in range(self.n_cards):
             mask[i] = np.reshape(from_vec[i*35: i*35 + 25], (5,5))
@@ -91,7 +65,7 @@ class Belief():
             card /= card.sum()
 
 
-    def encode(self, obs, player_id):
+    def encode(self, vectorized, player_id):
         """
         Encodes the observations and the knowledge into the belief
             player (int) 
@@ -105,26 +79,25 @@ class Belief():
         # of all non-common cards (fireworks and discard)
         self.reset()
 
-        # update the firework knowledge
-        fireworks = obs['fireworks']
-        for color in fireworks:
-            if fireworks[color] != 0:
-                # insert in the last spot
-                self.insert_many(self.n_cards *
-                                 self.players, color, fireworks[color])
+        translated = state_translator(vectorized, self.players)
+
+        # update the fireworks knowledge
+        fireworks = np.asarray(translated.boardSpace)
+        fireworks = np.reshape(fireworks, (5,5))
+        index = self.n_cards * self.players
+        self.belief[index] = fireworks
 
         # update the discard knowledge
-        discard = obs['discard_pile']
-        for card in discard:
-            self.insert_discard(card)
+        discard = np.asarray(translated.discardSpace)
+        discard = np.reshape(discard, (5,10))
+        self.discard_belief = discard
 
         # update the observed hand knowledge
-        ob = obs['vectorized']
-        from_vec = ob[:25*self.n_cards*(self.players - 1)]
+        from_vec = vectorized[:25*self.n_cards*(self.players - 1)]
         mask = np.empty((self.n_cards,5,5), dtype=np.uint8)
         for i in range(self.n_cards * (self.players - 1)):
             self.belief[i + self.n_cards] = np.reshape(from_vec[i*25:(i+1)*25], (5,5))
 
-        self.calculate_prob(player_id, ob)
+        self.calculate_prob(player_id, vectorized)
 
         return self.belief[:self.n_cards]
