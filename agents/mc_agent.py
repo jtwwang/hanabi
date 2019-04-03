bf80ac937f9d66e3adef631779fa73f3a1c3bc07
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-04-03 09:48:31 -0700

    work in progress - implemented first move

--- agents/mc_agent.py
+++ agents/mc_agent.py
@@ -1,20 +1,22 @@
 # Artificial Intelligence Society at UC Davis
 #
-#   http://www,aidavis.org
+#   http://www.aidavis.org
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
-"""A script to collect episodes from simple agent"""
+"""A script that implements a Monte Carlo Tree search"""
 
 from rl_env import Agent
 import numpy as np
 import baysian_belief
 import random
+import pyhanabi
+from copy import copy, deepcopy
 from policy_predictor import policy_net
 from datetime import datetime
 random.seed(datetime.now())
-
+from state_translate import state_translator
 
 class MCAgent(Agent):
     """Agent that uses monte carlo tree search to find the optimal move"""
@@ -45,56 +47,91 @@ class MCAgent(Agent):
                 else:
                     rand -= card[r][c]
 
-    def sampled_obs(self, my_belief, ob):
-        """ returns a sample of observations from belief and observations"""
-        v_sample = np.zeros(0)
-        for card in my_belief:
-            v = self.sample(card)
-            v_sample = np.append(v_sample,v)
+    def sampled_vector(self, vectorized):
+        """ 
+        returns a sample of observations from belief and observations
+        Args:
+            vectorized (list): the observations in a vectorized form
+        """
+        v_sample = []
+        translated = state_translator(vectorized, self.config['players'])
+        print(self.my_belief.shape)
+        card = self.my_belief[0]
+        for i in range(self.my_belief.shape[0]):
+            v = self.sample(card).tolist()
+            v_sample = v_sample + v
+            print(len(v_sample))
+            #self.my_belief = 
 
-        raise NotImplementedError
+        print(len(translated.handSpace))
+
         return v_sample
-
-    def next_state_from_move(self, move, state):
-        """returns the next state given a move"""
-        raise NotImplementedError
 
     def encode(self, ob):
         """returns an hashable state from the observation"""
         return str(ob)
 
-    def decode(self, state):
-        raise NotImplementedError
+    def update_visits(self, state):
+        """
+        This function updates the visits statics for a certain state
+        Args: state (string) the encoded representation of a string
+        """
+        if state not in self.stats.keys():       # update_node
+            self.stats[state] = {'visits': 1, 'value' : 0}
+        else:
+            self.stats[state]['visits'] += 1
 
-    def next_state_from_predictor(self, state):
-        raise NotImplementedError
+    def act(self, obs, env):
+        self.stats = {} # create empty dictionary to save stats of all nodes
 
-    def act(self, obs):
-        my_belief = self.belief.encode(obs, self.player_id)
-        
-        # create empty dictionary to save stats of all nodes
-        stats = {}
-
-        # set initial state, make it hashable
-        self.root = self.encode(obs['vectorized'])
-        state = self.root
-
+        self.root = env.state               # set the root from the state of the game
+        vectorized = env.observation_encoder.encode(self.root.observation(self.player_id))
+        state = self.encode(vectorized)     # make it hashable
+        self.update_visits(state)           # increment number of visits
+       
         # random rollouts
-        rollouts = 1000
-        for r in range(rollouts):
-            depth = 0
-            while depth < self.max_depth:
-                # my move
-                move = random.choice(obs['legal_moves'])
-                state = self.next_state_from_move(move, state)
+        rollouts = 100
+        for r in range(rollouts):   
+            depth = 0                       # reset the depth
+            history = []                    # reset the history of the rollout
+            game_state = self.root          # reset the state of the rollout
 
-                # other players moves
-                for p in self.config['players']:
+            while depth < self.max_depth:
+
+                # choose random action
+                legal_moves = game_state.legal_moves()
+                move = random.choice(legal_moves)
+
+                nextHanabiState = game_state.copy() # make a copy
+                nextHanabiState.apply_move(move)    # make a move
+
+                # if it's a play or discard move, need to deal the cards
+                if nextHanabiState.cur_player() == pyhanabi.CHANCE_PLAYER_ID:
+                    nextHanabiState.deal_random_card()
+
+                vectorized = env.observation_encoder.encode(nextHanabiState.observation(self.player_id))
+                state = self.encode(vectorized)     # make it hashable
+                self.update_visits(state)           # increment number of visits
+                history.append(state)               # append the state to the history of the rollout
+
+                # set my belief
+                self.my_belief = self.belief.encode(obs, self.player_id)
+
+                # other players' moves
+                for p in range(self.config['players']):
                     # choose random sample
-                    state = self.next_state_from_predictor(state)
+                    vectorized = self.sampled_vector(vectorized)
+                    # predict the move
+
+                    # if move is hint, calculate the score and terminate
+
+                    # else, make the move
 
                 # increase the depth by 1
                 depth += 1
+            
+            for i in range(len(history)):
+                pass
         
         return random.choice(obs['legal_moves']) #FIXME: 
 
