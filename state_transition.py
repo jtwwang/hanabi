370952e13d8e4102d9f2dfc6978c7709a1b828df
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-04-17 17:13:14 -0700

    some edits to start transition to 2.0

---
+++ state_transition.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def apply_move(state, move):
+    """
+    Args:
+        state (list): an array of all the information of the game,
+        which include observation and belief of the agebt
+        move: the action taken by the agent
+    returns:
+        the state after the action.
+    """
+
