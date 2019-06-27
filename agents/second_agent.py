5dfc121c8f9ad66cbc4e958c6c8c9e5214bef29b
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-06-26 18:46:59 -0700

    improved second_agent

--- agents/second_agent.py
+++ agents/second_agent.py
@@ -15,6 +15,8 @@
 
 from rl_env import Agent
 
+global colors
+colors = ['Y','B','W','R','G']
 
 class SecondAgent(Agent):
     """Agent that applies a simple heuristic."""
@@ -25,24 +27,35 @@ class SecondAgent(Agent):
         # Extract max info tokens or set default to 8.
         self.max_information_tokens = config.get('information_tokens', 8)
 
-    @staticmethod
-    def playable_card(card, fireworks):
+    def playable_card(self, card, fireworks):
         """A card is playable if it can be placed on the fireworks pile."""
-        return card['rank'] == fireworks[card['color']]
+        if card['color'] == None and card['rank'] != None:
+            for color in colors:
+                if fireworks[color] == card['rank']:
+                    continue
+                else:
+                    return False
+                
+            return True
+        elif card['color'] == None or card['rank'] == None:
+            return False
+        else:
+            return card['rank'] == fireworks[card['color']]
 
     def act(self, observation):
         """Act based on an observation."""
         if observation['current_player_offset'] != 0:
             return None
 
+        fireworks = observation['fireworks']
+
         # Check if there are any pending hints and play the card corresponding to
         # the hint.
         for card_index, hint in enumerate(observation['card_knowledge'][0]):
-            if hint['color'] is not None or hint['rank'] is not None:
+            if self.playable_card(hint, fireworks):
                 return {'action_type': 'PLAY', 'card_index': card_index}
 
         # Check if it's possible to hint a card to your colleagues.
-        fireworks = observation['fireworks']
         if observation['information_tokens'] > 0:
             # Check if there are any playable cards in the hands of the opponents.
             for player_offset in range(1, observation['num_players']):
@@ -50,14 +63,14 @@ class SecondAgent(Agent):
                 player_hints = observation['card_knowledge'][player_offset]
                 # Check if the card in the hand of the opponent is playable.
                 for card, hint in zip(player_hand, player_hints):
-                    if SecondAgent.playable_card(card,
+                    if self.playable_card(card,
                                                  fireworks) and hint['color'] is None:
                         return {
                             'action_type': 'REVEAL_COLOR',
                             'color': card['color'],
                             'target_offset': player_offset
                         }
-                    elif SecondAgent.playable_card(card, fireworks) and hint['rank'] is None:
+                    elif self.playable_card(card, fireworks) and hint['rank'] is None:
                         return {
                                 'action_type': 'REVEAL_RANK',
                                 'rank': card['rank'],
