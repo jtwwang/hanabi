1e2026009c4aba4764c0f1c7d3f1bf1521ab8801
LorenzoM1997 <lorenz.m97@gmail.com>
2019-09-07 22:58:07 -0700

    adapt ensemble to new code, use new accuracy function

--- predictors/ensemble.py
+++ predictors/ensemble.py
@@ -13,26 +13,23 @@
 from .conv_pred import conv_pred
 from os import listdir, path
 from tensorflow.keras.models import load_model
-from tensorflow.keras.metrics import CategoricalAccuracy
-from tensorflow.keras.layers import Softmax
-import numpy as np
-import tensorflow as tf
+from data_pipeline.util import accuracy
 
-tf.enable_eager_execution()
+import numpy as np
+
 
 class ensemble(conv_pred):
 
     def __init__(self, agent_class, predictor_name):
 
         # initialize
-        model_dir = path.join("model", agent_class)
-        model_dir = path.join(model_dir, "conv")
+        model_dir = path.join("model", "conv")
 
         # open the path
         files = listdir(model_dir)
-        print("Building an ensemble of %i networks" %(len(files)))
+        print("Building an ensemble of %i networks" % (len(files)))
 
-        self.models = [] # initialize empty list of models
+        self.models = []  # initialize empty list of models
         self.files = []
         for file in files:
             pred_dir = path.join(model_dir, file)
@@ -47,26 +44,21 @@ class ensemble(conv_pred):
 
         self.X_test = self.reshape_data(self.X_test)
 
-        steps = int(self.X_test.shape[0]/batch_size)
+        pred = self.prediction(self.X_test, batch_size)
+        acc = accuracy(pred, self.y_test)
+        print('Final result: %f' % acc)
 
-        acc = CategoricalAccuracy()
-        single_acc = CategoricalAccuracy()
+    def predict(self, X, batch_size=64):
         avg_pred = np.zeros(self.y_test.shape)
         for i in range(len(self.files)):
-            #model = load_model(self.files[i])
             pred = self.models[i].predict(
-                x=self.X_test,
+                x=X,
                 batch_size=batch_size,
                 verbose=0)
             avg_pred += pred
-            single_acc.update_state(self.y_test, pred)
-            print(" * "+self.files[i]+" : "+str(single_acc.result().numpy()))
-            single_acc.reset_states()
-
-        # compute accuracy
         avg_pred /= len(self.files)
-        acc.update_state(self.y_test, avg_pred)
-        print('Final result: %f' %acc.result().numpy())
+
+        return avg_pred
 
     def save(self):
         print("Model is not saved because it is an ensemble.")
